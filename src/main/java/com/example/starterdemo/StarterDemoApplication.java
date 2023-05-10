6764d647e9744e638060c5ada992c75abf4a9bf4
caozhixin <593721042@qq.com>
2023-05-10 10:57:50 +0800

    坚持不懈

--- src/main/java/com/example/starterdemo/StarterDemoApplication.java
+++ src/main/java/com/example/starterdemo/StarterDemoApplication.java
@@ -1,8 +1,10 @@
 package com.example.starterdemo;
 
+import com.example.starterdemo.anno.EnableAutoConfigTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfigTest
 @SpringBootApplication
 public class StarterDemoApplication {
 
--- src/main/java/com/example/starterdemo/demo/AutoConfigurationTest.java
+++ src/main/java/com/example/starterdemo/demo/AutoConfigurationTest.java
@@ -1,9 +1,12 @@
 package com.example.starterdemo.demo;
 
+import com.example.starterdemo.anno.EnableAutoConfigTest;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnBean(annotation = EnableAutoConfigTest.class)
 public class AutoConfigurationTest {
 
     @Bean
