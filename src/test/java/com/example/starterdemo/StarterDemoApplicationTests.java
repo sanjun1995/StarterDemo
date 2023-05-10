f669f5deb245183cf8f750308806f44dfe767309
caozhixin <593721042@qq.com>
2023-05-10 14:28:41 +0800

    坚持不懈

--- src/test/java/com/example/starterdemo/StarterDemoApplicationTests.java
+++ src/test/java/com/example/starterdemo/StarterDemoApplicationTests.java
@@ -1,9 +1,6 @@
 package com.example.starterdemo;
 
-import com.example.starterdemo.anno.EnableAutoConfigTest;
 import com.example.starterdemo.demo.ServiceBean;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
